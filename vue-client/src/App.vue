cca7a9e0d18ae4f8434ed38547f3d9757e710215
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-05-04 20:51:26 +0300

    Register implementation

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -3,13 +3,77 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
  
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
- 
-  <RouterView />
+  <main>
+    <header class="p-3 text-bg-dark">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li>
+              <RouterLink to="/" class="nav-link px-2 text-secondary">Home</RouterLink>
+            </li>
+
+          </ul>
+          <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">Login</button>
+            <RouterLink :to="{ name: 'register' }" class="btn btn-warning">Register</RouterLink>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <RouterView />
+  
+  </main>
 </template>
+
+<style>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.b-example-divider {
+  height: 3rem;
+  background-color: rgba(0, 0, 0, .1);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+
+.b-example-vr {
+  flex-shrink: 0;
+  width: 1.5rem;
+  height: 100vh;
+}
+
+.bi {
+  vertical-align: -.125em;
+  fill: currentColor;
+}
+
+.nav-scroller {
+  position: relative;
+  z-index: 2;
+  height: 2.75rem;
+  overflow-y: hidden;
+}
+
+.nav-scroller .nav {
+  display: flex;
+  flex-wrap: nowrap;
+  padding-bottom: 1rem;
+  margin-top: -1px;
+  overflow-x: auto;
+  text-align: center;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}</style>
--- vue-client/src/router/index.js
+++ vue-client/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
- 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,7 +8,13 @@ const router = createRouter({
       name: 'home',
       component: import('../views/HomeView.vue'),
     },
+
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/Auth/RegisterView.vue"),
+    },
   ],
 })
- 
+
 export default router
